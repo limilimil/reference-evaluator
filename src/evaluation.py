5b00fdccc8bec6760a4757d3b7c8a77e7cb5ba48
lcann <liamcannon93@gmail.com>
2025-07-25 23:45:36 +0100

    Added comments

--- src/evaluation.py
+++ src/evaluation.py
@@ -220,7 +220,7 @@ class LevenshteinVolumeEvaluator(LevenshteinEvaluator):
             return "N/A"
         return rapidfuzz.distance.Levenshtein.normalized_similarity(src.strip(), ext.strip())
 
-
+# Dict of evaluators for each Reference attribute
 evaluator_registry = {
     "title": {
         "boolean": BooleanTitleEvaluator(),
@@ -245,76 +245,114 @@ evaluator_registry = {
     }
 }
 
-
+"""
+Manages evaluation inputs and outputs
+Attributes:
+    config(dict):
+        JSON formatted dictionary for setting evaluation methods for attributes
+Methods:
+    evaluate_element(element, src_ref, ext_ref):
+        Evaluates element using evaluation method specified in config
+    aggregate(evaluations):
+        Combines evaluation scores using a weighted average
+    evaluate(src_ref, ext_ref):
+        Evaluates attributes of a single Reference instance
+"""
 class EvaluationController:
     def __init__(self, config):
         self.config = config
 
+    """
+    Evaluates element using evaluation method specified in config
+    Parameters:
+        element (str): Name of the attribute to be evaluated
+        src_ref (Reference): Source reference
+        ext_ref (Reference): External reference
+    Returns:
+        dict: Formatted response with evaluation details
+    """
     def evaluate_element(self, element, src_ref, ext_ref):
-        src_elem = getattr(src_ref, element)
+        src_elem = getattr(src_ref, element) # Gets attribute from Reference instance
         ext_elem = getattr(ext_ref, element)
-        eval_weights = self.config[element]["evaluators"]
+        eval_weights = self.config[element]["evaluators"] # Retrieves method-weight key-value pair
         evaluations = []
 
         for method, weight in eval_weights.items():
-            evaluator = evaluator_registry.get(element).get(method)
-            result = evaluator.evaluate(src_elem, ext_elem)
-            result['weight'] = weight
+            evaluator = evaluator_registry.get(element).get(method) # Gets specific evaluator for named element
+            result = evaluator.evaluate(src_elem, ext_elem) # Calls the evaluators evaluate method
+            result['weight'] = weight # Stores the weight in the results
             evaluations.append(result)
 
         if len(evaluations) == 1:
-            score = evaluations[0]["score"]
+            score = evaluations[0]["score"] # Scores don't need aggregating if only one evaluator is utilised
         else:
             score = self.aggregate(evaluations)
 
         return {
-            "score": score,
+            "score": score, # Total score of all evaluations of element combined
             "evaluation-method": evaluations
         }
 
+    """
+    Combines evaluation scores using a weighted average
+    Parameters:
+        evaluations (list[dict]): Collection of evaluation result dictionaries
+    Returns:
+        float: normalised weighted average of scores
+    """
     def aggregate(self, evaluations):
         scores = []
         weights = []
 
         for e in evaluations:
             score = e['score']
-            weight = e.get('weight', 1.0)
-            if isinstance(score, float):
+            weight = e.get('weight', 1.0) # Defaults to 1.0 if no weight exists
+            if isinstance(score, float): # Skips "N/A" strings
                 scores.append(score)
                 weights.append(weight)
 
         return fmean(scores, weights=weights) if weights else None
 
+    """
+    Evaluates attributes of a single Reference instance
+    Parameters:
+        src_ref (Reference): Source Reference
+        ext_ref (Reference): External Reference
+    Returns:
+        dict: Nested dictionary of evaluation result strings
+    """
     def evaluate(self, src_ref, ext_ref):
         results = {}
         for elem in self.config:
             results[elem] = self.evaluate_element(elem, src_ref, ext_ref)
-        overall = all(results[elem]["score"] for elem in results)
+        overall = all(results[elem]["score"] for elem in results) # overall combined score of all attribute scores
 
-        return {"overall": overall, "reference element": results}
+        return {"overall": overall, "reference element": results} # list of attributes and their evaluations
 
 
 """
 Run full evaluator
 Parameters:
-bibliography (BeautifulSoup): reference XML file as a bs object
-config (dict): evaluation configuration
-mailto (str): email address required for crossref api
-file_name (str): name of output file (optional)
+    bibliography (BeautifulSoup): XML file of bibliography as soup parser object
+    config (dict): Evaluation configuration
+    mailto (str): Email address required for crossref api
+    file_name (str): Name of output file (optional)
+Returns:
+    dict: all reference evaluations
 """
 def evaluate_bibliography(bibliography, config, mailto, file_name=""):
-    evaluator = EvaluationController(config)
-    parsed_bib = parser.XmlBibliography().parse(bibliography)
+    evaluator = EvaluationController(config) # Load evaluation settings onto controller
+    parsed_bib = parser.XmlBibliography().parse(bibliography) # Parses into Reference objects
     results = []
     for ref in parsed_bib:
-        search_results = crossref.CrossrefSearcher(mailto, 20).search(ref)
+        search_results = crossref.CrossrefSearcher(mailto, 20).search(ref) # Initiates search
         if search_results is not None:
             found_ref = crossref.CrossrefParser().extract_ref(search_results)
             evaluation = evaluator.evaluate(ref, found_ref)
             results.append({'reference': ref, 'reference-located': found_ref, 'evaluation': evaluation})
         else:
-            results.append({'reference': ref, 'reference-located': 'None Found', 'evaluation': 'None'})
+            results.append({'reference': ref, 'reference-located': 'None Found', 'evaluation': 'None'}) # If no reference is found
 
     print("finished, returning results")
-    utils.export_json(results, file_name + (" - " if len(file_name) > 0 else "") + "verification results")
+    utils.export_json(results, file_name + (" - " if len(file_name) > 0 else "") + "verification results") # exports results as JSON
     return results
